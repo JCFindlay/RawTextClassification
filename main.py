7132675d19e50f6c0e7fe89a3ee48973edf3abe0
JCFindlay <joe.fin.bus@gmail.com>
2025-08-16 22:58:49 +0100

    changes to main

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ print("PyTorch version:", torch.__version__)
 t1 = torch.tensor([1, 2, 3])
 t2 = torch.tensor([4, 5, 6])
 
-print("Tensor 1:", t1)
-print("Tensor 2:", t2)
 
 output = torch.matmul(t1, t2)
 print("Output of matmul:", output)
