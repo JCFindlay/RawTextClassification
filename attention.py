33dbb964a5550432059837382730c6569134797a
JCFindlay <joe.fin.bus@gmail.com>
2025-08-17 12:09:38 +0100

    attention

--- attention.py
+++ attention.py
@@ -1,5 +1,24 @@
 import torch
+import numpy as np
+import math
 
-def scaled_dot_product_attention(query, key, value):
-    pass 
+def scaled_dot_product_attention(query: torch.tensor, key: torch.tensor, value: torch.tensor):
+    
+    #matmul 
+    q_k_result = torch.matmul(query, torch.transpose(key, -2, -1))
+    # these above should both be tensors
+    
+    
+    scaled = q_k_result/math.sqrt(query.shape[-1])
+    
+    # Softmax
+    
+    sm = torch.nn.Softmax(dim=-1)
+    attention_matrix = sm(scaled)
+    
+    # Last matmul
+    
+    s_d_p_attention = torch.matmul(attention_matrix, value)
+    
+    return s_d_p_attention 
     
--- main.py
+++ main.py
@@ -1,10 +1,34 @@
 import torch
 
+import attention as at
+
 print("PyTorch version:", torch.__version__)
 
-t1 = torch.tensor([1, 2, 3])
-t2 = torch.tensor([4, 5, 6])
+t1 = torch.tensor([[1, 2, 3],[4,5,6]])
+t2 = torch.tensor([[4, 5, 6], [6,7,8]])
+
+print(torch.transpose(t2,0,1))
+
+print(torch.matmul(t1,torch.transpose(t2,0,1)))
 
 
-output = torch.matmul(t1, t2)
-print("Output of matmul:", output)
+print(t2.ndim)
+
+print(t2.shape)
+
+Q = torch.tensor([[1.0, 0.0, 1.0],
+                  [0.0, 1.0, 0.0]])
+
+# Keys same size as queries
+K = torch.tensor([[1.0, 0.0, 0.0],
+                  [0.0, 1.0, 1.0]])
+
+# Values also have dimension 3
+V = torch.tensor([[1.0, 2.0, 3.0],
+                  [4.0, 5.0, 6.0]])
+
+print(at.scaled_dot_product_attention(Q,K,V))
+
+
+
+
